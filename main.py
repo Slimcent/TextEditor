cae2a612bd4dd89863e634e77083aec2dac8107c
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2023-03-12 17:31:51 +0100

    Added edit menu item

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import tkinter as tk
+from tkinter import filedialog
 
 
 def openfile():
-    print("File Open")
+    filename = filedialog.askopenfilename(initialdir="/", title="Open File",
+                                          filetypes=(("Text Files", "*.txt"), ("All Files", "*.*")))
+    print(filename)
 
 
 form = tk.Tk()
@@ -10,8 +13,8 @@ form.title("Python Menus")
 
 menuBar = tk.Menu(form)
 
+#  File menu
 fileMenuItems = tk.Menu(menuBar, tearoff=0)
-
 fileMenuItems.add_command(label="Open", command=openfile)
 fileMenuItems.add_command(label="Save", command=openfile)
 fileMenuItems.add_command(label="Save As", command=openfile)
@@ -21,7 +24,21 @@ fileMenuItems.add_command(label="Quit", command=form.quit)
 
 menuBar.add_cascade(label="File", menu=fileMenuItems)
 
+#  Edit menu
+editMenu = tk.Menu(menuBar, tearoff=0)
+editMenu.add_command(label="Cut")
+editMenu.add_command(label="Copy")
+editMenu.add_command(label="Paste")
+
+menuBar.add_cascade(label="Edit", menu=editMenu)
+
 form.config(menu=menuBar)
+
+textArea = tk.Text(form, height=12, width=80, wrap=tk.WORD)
+textArea.pack()
+
+textArea.insert(tk.END, 'Some default text here')
+textArea.configure(font=("Arial", 14, "bold", "italic"))
 
 form.mainloop()
 
